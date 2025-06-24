3eb5bedd12df3fe93b288d17f8c3ed05177185c7
alexshehg <96130112+alexshehg@users.noreply.github.com>
2025-06-24 23:41:34 +0300

    Update test_items.py
    Добавлен assert

--- test_items.py
+++ test_items.py
@@ -7,3 +7,4 @@ def test_visible_button_add_to_cart(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     time.sleep(10)
     button = browser.find_element("css selector", ".btn-add-to-basket")
+    assert button is not None, "Кнопки 'Добавить в корзину' нет!"
