8eaf92cfc83bef872f50d43e4ecd9a8eb16b46bb
alexshehg <96130112+alexshehg@users.noreply.github.com>
2025-06-24 23:19:18 +0300

    Create conftest.py
    Добавлен файл с конфигураций тестов
    В нем конфигурация выбора браузера и языка

---
+++ conftest.py
@@ -0,0 +1,21 @@
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+
+    browser = None
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        options = Options()
+        options.set_preference("intl.accept_languages", user_language)
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(options=options)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
