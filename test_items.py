380fda55f6eb69e46ec49b35865ca30e99bc4439
alexshehg <96130112+alexshehg@users.noreply.github.com>
2025-06-24 23:22:24 +0300

    Update test_items.py
    Смена таймаута с 30 до 10 секунд

--- test_items.py
+++ test_items.py
@@ -5,5 +5,5 @@ from selenium.webdriver.common.by import By
 
 def test_visible_button_add_to_cart(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    time.sleep(30)
+    time.sleep(10)
     button = browser.find_element("css selector", ".btn-add-to-basket")
